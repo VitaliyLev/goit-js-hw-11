47a4227c98c8ca2095cf959eeacbd32a5a14b746
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-11-15 14:54:29 +0200

    add html mark

--- src/index.js
+++ src/index.js
@@ -1,53 +1,96 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const MY_PIXA_KEY = '31278796-a3a5484ed91accb8b7bce1cf7';
-
-const URL = 'https://pixabay.com/api/?key=';
+// Notify.success('');
+// Notify.warning('Sorry, there are no images matching your search query. Please try again.');
 
 const refs = {
-  //   searchel: document.querySelector('.searchbtn'),
   markupEl: document.querySelector('.gallery'),
   formSubmitEl: document.querySelector('#search-form'),
+  imgPlus: document.querySelector('.searchbtns'),
 };
 
-async function getImage(searh) {
-  try {
-    const result = await axios.get(
-      URL +
-        MY_PIXA_KEY +
-        `&q=${searh}&image_type=photo&orientation=horizontal&safesearch=true`
-    );
-    return result.data;
-  } catch (error) {
-    console.log(error);
+const URL = 'https://pixabay.com/api/?key=';
+const MY_PIXA_KEY = '31278796-a3a5484ed91accb8b7bce1cf7&';
+const perPage = 40;
+
+class ApiImagesRequest {
+  constructor() {
+    this.url = URL;
+    this.key = MY_PIXA_KEY;
+    this.perPage = perPage;
+    this.search = '';
+    this.page = 1;
+  }
+
+  get searchValue() {
+    return this.search;
+  }
+
+  set searchValue(value) {
+    return (this.search = value);
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  getOptions() {
+    const options = new URLSearchParams({
+      q: `${this.search}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: `${this.perPage}`,
+      page: `${this.page}`,
+    });
+    return options;
+  }
+
+  async getImages() {
+    const option = this.getOptions();
+    const response = await axios.get(`${URL}${MY_PIXA_KEY}${option}`);
+    return response.data;
   }
 }
 
+const responseApiImg = new ApiImagesRequest();
+
 function handleSubmit(e) {
   e.preventDefault();
-  let text = e.target.elements.searchQuery.value;
-  // let textt = e.currentTarget.elements.searchQuery.value;
-  console.log(text);
+  refs.markupEl.innerHTML = '';
+  responseApiImg.resetPage();
+  responseApiImg.searchValue = e.target.elements.searchQuery.value.trim();
+  // e.target.elements.searchQuery.value = responseApiImg.searchValue;
 
-    // getImage(text).then(res => {
-    //   renderImgMurkup(res);
-    // //   console.log(res);
-    // }).catch(er => console.log(er.message));
+  if (responseApiImg.searchValue === '') {
+    Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
 
-  const markup = async () => {
-    try {
-      const mark = await getImage(text);
-      renderImgMurkup(mark);
-    } catch (error) {
-      console.log(error.message);
+  responseApiImg.getImages().then(image => {
+    if (image.hits.length === 0) {
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
     }
-  };
-  markup();
+  });
+
+  renderImgMarkup();
+  responseApiImg.incrementPage();
 }
 
 refs.formSubmitEl.addEventListener('submit', handleSubmit);
 
-function renderImgMurkup({ hits }) {
+//рендер розмітки html
+function renderHtmlMurkup({ hits }) {
   const markup = hits
     .map(
       ({
@@ -75,43 +118,21 @@ function renderImgMurkup({ hits }) {
   refs.markupEl.insertAdjacentHTML('beforeend', markup);
 }
 
-// function renderMarkup({ hits }) {
-//     const markup = hits
-//       .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-//         return `
-//         <a class="gallery__link" href="${largeImageURL}">
-//           <div class="photo-card">
-//               <img src="${webformatURL}" width="305" height="205" alt="${tags}" loading="lazy" />
-//               <div class="info">
-//                 <p class="info-item">
-//                   <b>Likes</b>
-//                   ${likes}
-//                 </p>
-//                 <p class="info-item">
-//                   <b>Views</b>
-//                   ${views}
-//                 </p>
-//                 <p class="info-item">
-//                   <b>Comments</b>
-//                   ${comments}
-//                 </p>
-//                 <p class="info-item">
-//                   <b>Downloads</b>
-//                   ${downloads}
-//                 </p>
-//               </div>
-//           </div>
-//           </a>
-//           `;
-//       })
-//       .join('');
-//     refs.markupEl.insertAdjacentHTML('beforeend', markup);
-//   }
+function handlePlus(e) {
+  e.preventDefault();
+  renderImgMarkup();
+  responseApiImg.incrementPage();
+}
 
-// webformatURL - посилання на маленьке зображення для списку карток.
-// largeImageURL - посилання на велике зображення.
-// tags - рядок з описом зображення. Підійде для атрибуту alt.
-// likes - кількість лайків.
-// views - кількість переглядів.
-// comments - кількість коментарів.
-// downloads
+refs.imgPlus.addEventListener('click', handlePlus);
+
+//функція обробки відповіді з сервера
+async function renderImgMarkup() {
+  try {
+    const mark = await responseApiImg.getImages();
+    // return renderImgMurkup(mark);
+    renderHtmlMurkup(mark);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
